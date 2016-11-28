e81b8c9368639cda933b4ba135cff8f39681e8ea
manishsibanjar123 <manish.sibanjar123@gmail.com>
2016-11-28 12:47:59 +0545

    Add files via upload

---
+++ square root.c
@@ -0,0 +1,22 @@
+/*
+Student name : Manish sibanjar
+Subject : programming fundamental
+Lab sheet no : 07
+Program : to calculate square
+Date : 28 nov,2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+int main (){
+     int  a,squareroot;
+printf("Enter any number\n");
+scanf("%d",&a);
+squareroot =sqrt(a);
+printf("the square root is : %d\n",squareroot);
+getch();
+return 0;
+}
+

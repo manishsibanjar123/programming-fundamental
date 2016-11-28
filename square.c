563dc63995b66b198edc9634520d3bc47fdd17a6
manishsibanjar123 <manish.sibanjar123@gmail.com>
2016-11-28 12:38:05 +0545

    Add files via upload

---
+++ square.c
@@ -0,0 +1,21 @@
+/*
+Student name : Manish sibanjar
+Subject : programming fundamental
+Lab sheet no : 07
+Program : to calculate square
+Date : 28 nov,2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main (){
+     int a,square;
+printf("Enter any number\n");
+scanf("%d",&a);
+square = a*a;
+printf("the square is : %d\n",square);
+getch();
+return 0;
+}
+

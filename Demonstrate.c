d150febe04344d7d1e26dfd015a41974fc8f3d44
manishsibanjar123 <manish.sibanjar123@gmail.com>
2016-12-02 12:35:27 +0545

    Add files via upload

---
+++ Demonstrate.c
@@ -0,0 +1,25 @@
+/*
+Student name : Manish Sibanjar
+Subject : Programming fundamental
+Lab sheet no : 09
+Program : WAP to demonstrate increament and decreament
+Date : 2 Dec,2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main () {
+int a;
+
+printf("Enter the first number\n");
+scanf("%d",&a);
+
+printf("the post increament is :%d\n",a++);
+printf("the pre increament is :%d\n",++a);
+
+getch();
+return 0;
+}
+
+

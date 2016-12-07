2ae7ad1dc406d56353983263a03a42adabb03cc0
manishsibanjar123 <manish.sibanjar123@gmail.com>
2016-12-07 11:46:04 +0545

    Add files via upload

---
+++ ternary.c
@@ -0,0 +1,21 @@
+/*
+Student name : Manish Sibanjar
+Subject : Programming fundamental
+Lab sheet no : 10
+Program : WAP to find odd or even using ternary operator
+Date : 7 Dec,2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main () {
+    int a;
+printf("Enter the number\n");
+scanf("%d",&a);
+(a%2==0)?
+printf("The number is even\n"):
+printf("The number is odd\n");
+getch ();
+return 0;
+}

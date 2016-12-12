ea821f05b442a1e6df1ae99157e8a2c9cdf30019
manishsibanjar123 <manish.sibanjar123@gmail.com>
2016-12-12 11:10:44 +0545

    Add files via upload

---
+++ largest using ternary.c
@@ -0,0 +1,27 @@
+/*
+Student name : Manish Sibanjar
+Subject : programming fundamental
+Roll no : 17
+Lab sheet no: 11
+Program : WAP to find largest number using ternary operator
+Date : 12 Dec,2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main () {
+    int a,b;
+printf("Enter the first number\n");
+scanf("%d",&a);
+printf("Enter the second number\n");
+scanf("%d",&b);
+
+(a>=b)?
+printf("%d is the largest number\n",a):
+printf("%d is the largest number\n",b);
+getch ();
+return 0;
+}
+
+

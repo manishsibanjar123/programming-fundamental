b1adc20719e37f7d415f72f31c65dbbaf9938f83
manishsibanjar123 <manish.sibanjar123@gmail.com>
2017-01-09 10:43:49 +0545

    Add files via upload

---
+++ odd or even using ternary.c
@@ -0,0 +1,20 @@
+/*
+Student name : Manish sibanjar
+Subject : Programming fundamental
+Lab sheet no : 14
+Roll no : 21
+Program : WAP to check no. is odd or even using ternary operator
+Date : 8 jan,2017
+*/
+
+#include<stdio.h>
+
+int main(){
+	int a;
+printf("Enter any number\n");
+scanf("%d",&a);
+(a%2==0)?printf("The number is even\n"):printf("The number is odd\n");
+
+return 0;
+
+}

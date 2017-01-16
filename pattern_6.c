242182a21c3ea203927ba4a512a8d078988a2a5a
manishsibanjar123 <manish.sibanjar123@gmail.com>
2017-01-16 12:32:11 +0545

    Add files via upload

---
+++ pattern_6.c
@@ -0,0 +1,23 @@
+/*
+Student name : Manish Sibanjar
+Subject : programming fundamental
+Roll no : 21
+Lab sheet no : 19
+Program : WAP to display the number 
+Date : 16 jan,2017
+*/
+
+#include<stdio.h>
+
+int main (){
+	int i,j;
+	for(i=1;i<=2;i++)
+	{
+		for(j=1;j<=3;j++)
+		{
+			printf("1");
+		}
+		printf("\n");
+	}
+	return 0;
+}

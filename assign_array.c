466d3a1f04ea98bb2afcc21b650c20ecf336781b
manishsibanjar123 <manish.sibanjar123@gmail.com>
2017-02-01 10:44:09 +0545

    Add files via upload

---
+++ assign_array.c
@@ -0,0 +1,18 @@
+/*
+Student Name : Manish Sibanjar
+Subject : Programming fundamental
+Lab sheet no :23
+Program : WAP to assign the value in array
+Date : 1 feb,2017
+*/
+
+#include<stdio.h>
+int main(){
+	int a[5]={1,2,3,4,5},i;
+	for(i=0;i<5;i++)
+	{
+		printf("a[%d]=%d\n",i,a[i]);
+	}
+	return 0;
+}
+	

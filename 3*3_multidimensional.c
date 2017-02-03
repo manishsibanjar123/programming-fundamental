5b8b7711647f13672aa32c3c13f839561ace6ad6
manishsibanjar123 <manish.sibanjar123@gmail.com>
2017-02-03 12:27:21 +0545

    Add files via upload

---
+++ 3*3_multidimensional.c
@@ -0,0 +1,21 @@
+/*
+Student name : Manish Sibanjar
+Subject : Programming fundamental
+Lab sheet no : 25
+Program : WAP to declare and print multidimensional array
+Date : 3 feb,2017
+*/
+
+#include<stdio.h>
+int main (){
+	int array [3][3]={{1,2,3},{4,5,6},{7,8,9}},i,j;
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+	   printf("%d\t",array[i][j]);
+	    }
+	printf("\n");
+}
+return 0;
+}
